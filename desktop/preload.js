7281c9f3760a17723d640bce3453ecd167bd32f8
Your Name <you@example.com>
2026-02-21 23:31:35 +0400

    Emptying repository for re-upload

--- desktop/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("ratioApi", {
-  setWindowRatio: (ratio) => ipcRenderer.invoke("set-window-ratio", ratio),
-  clearWindowRatio: () => ipcRenderer.invoke("clear-window-ratio"),
-});
